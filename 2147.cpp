d8235544b64c8d6478f1fc2022d9e97a788e8a24
Naman Arora <122680076+namanarora2707@users.noreply.github.com>
2025-12-14 11:04:29 +0530

    Number of Ways to Divide a Long Corridor

---
+++ 2147.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int numberOfWays(string corridor) {
+    const int MOD = 1e9 + 7;
+    int totalSeats = 0;
+    for (char c : corridor) {
+        if (c == 'S') totalSeats++;
+    }
+    if (totalSeats == 0 || totalSeats % 2 != 0)
+        return 0;
+
+    long long ways = 1;
+    int seatCount = 0;
+    int plantCount = 0;
+    bool inBetween = false;
+
+    for (char c : corridor) {
+        if (c == 'S') {
+            seatCount++;
+            if (seatCount % 2 == 0) {
+                inBetween = true;
+                plantCount = 0;
+            }
+            else if (inBetween) {
+                ways = (ways * (plantCount + 1)) % MOD;
+                inBetween = false;
+            }
+        } 
+        else if (inBetween) {
+            plantCount++;
+        }
+    }
+
+    return ways;
+}
+int main() {
+    string corridor = "SSPPSPS";
+    cout << numberOfWays(corridor) << endl;
+    return 0;
+}
